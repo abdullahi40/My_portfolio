3c14d4fd8923ccfbcd5f010233d2d888c45d8f19
Abdullahi Suleiman <146683810+abdullahi40@users.noreply.github.com>
2025-06-23 10:40:31 +0300

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -48,6 +48,6 @@ app.use("/api", contactRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
